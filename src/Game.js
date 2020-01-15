b2d6b486060f50a4660fe3df6bf33a0a708fdc12
Eman <emmanuelgatwech@gmail.com>
2020-01-15 17:52:41 +0300

    enabled switching turns in the game

--- src/Game.js
+++ src/Game.js
@@ -1,57 +1,108 @@
 import $ from 'jquery'
-// import game from "./game.json";
 
 class Game {
   constructor (players, weapons, game) {
     this.game = game
     this.players = players
     this.weapons = weapons
-    this.currentTurn = 0
-    this.currentPlayer = null
-    this.currentLocation = null
-    this.nextLocation = null
-    this.allowedTop = []
-    this.allowedRight = []
-    this.allowedLeft = []
-    this.allowedBottom = []
+    this.active = null
+  }
+
+  movePlayer (oldPos, newPos) {
+    const oldCoords = this.getCoords(oldPos)
+    const newCoords = this.getCoords(newPos)
+
+    // remove class current player
+    $(oldPos).removeClass('current-player player')
+    console.log(oldPos)
+
+    // remove highlights
+
+    // add class player on newPos
+    $('.cell').removeClass("allowed player-on")
+    $(newPos).addClass('player')
+    console.log(newPos)
+
+    // switch turns
+    this.switchTurn(oldPos)
+  }
+
+  getPlayerPos (row, col) {
+    $(`[data-pos='(${parseInt(row) - parseInt(1)}, ${col})'], [data-pos='(${parseInt(row) - parseInt(2)}, ${col})']`).addClass('allowed')
+    $(`[data-pos='(${row}, ${parseInt(col) + parseInt(1)})'], [data-pos='(${row}, ${parseInt(col) + parseInt(2)})']`).addClass('allowed')
+    $(`[data-pos='(${parseInt(row) + parseInt(1)}, ${col})'], [data-pos='(${parseInt(row) + parseInt(2)}, ${col})']`).addClass('allowed')
+    $(`[data-pos='(${row}, ${parseInt(col) - parseInt(1)})'], [data-pos='(${row}, ${parseInt(col) - parseInt(2)})']`).addClass('allowed')
+  }
+
+  getCoords (player) {
+    const row = $(player).attr('data-row')
+    const col = $(player).attr('data-col')
+    return {
+      x: row,
+      y: col
+    }
+  }
+
+  // get allowed cells
+  switchTurn (player) {
+    const turn = $(player).attr('data-turn');
+    console.log(turn)
+    // $('.cell').removeClass('allowed')
+    console.log(parseInt(turn))
+    if(turn == 1) {
+      console.log('one')
+      this.active = $(`[data-turn='${parseInt(0)}']`).addClass('current-player')
+
+      const { x, y } = this.getCoords(this.active)
+      console.log(this.active)
+      // get player positions
+      this.getPlayerPos(x, y)
+    } else if(turn == 0) {
+      console.log('two')
+      this.active = $(`[data-turn='${parseInt(1)}']`).addClass('current-player')
+      const { x, y } = this.getCoords(this.active)
+      console.log(this.active)
+      // get player positions
+      this.getPlayerPos(x, y)
+    }
   }
 
   init () {
-    // get all cells
-    const gridItems = $(".cell")
+    let active
+    let past
+    let allowed
+    let self = this
 
-    // assign turn into player
-    const [currentPlayer] = $(".player")
+    // get players
+    const [first] = this.players
 
-    // highlight current player
-    $(currentPlayer).addClass('current-player')
+    // highlight active player
+    this.active = $(`[data-turn='${parseInt(1)}']`)
+    var attr = $(this.active).attr('data-name');
+    $(this.active).addClass('current-player')
+    $(this.active).attr('data-turn', first.turn)
 
     // get nearby cells
-    const row = $(currentPlayer).attr('data-row')
-    const col = $(currentPlayer).attr('data-col')
+    const { x, y } = this.getCoords(this.active)
 
-    // using a range in query selectors?
-    let topCell = $(`[data-pos='(${parseInt(row) - parseInt(1)}, ${col})']`).addClass('allowed')
-    this.allowedTop.push(topCell)
-    let rightCell = $(`[data-pos='(${row}, ${parseInt(col) + parseInt(1)})']`).addClass('allowed')
-    let bottomCell = $(`[data-pos='(${parseInt(row) + parseInt(1)}, ${col})']`).addClass('allowed')
-    let leftCell = $(`[data-pos='(${row}, ${parseInt(col) - parseInt(1)})']`).addClass('allowed')
+    // get player positions
+    this.getPlayerPos(x, y)
 
     // click events
-    $(gridItems).click(e => {
-      // $(e.target).addClass('unavailable')
-      console.log(e.target)
-
-      // one location or all possible locations?
-      // I can legit use a stack, add a last location at the top
-      this.currentLocation = $(e.target).attr('data-pos')
-      console.log(this.currentLocation)
-      console.log(this.allowedTop)
+    allowed = $('.allowed')
+    allowed.hover(function () {
+      // $(this).removeClass('allowed')
+      $(this).addClass('player-on')
+    }, function () {
+      // $(this).removeClass('allowed')
+      $(this).removeClass('player-on')
     })
 
-
+    allowed.click(function (e) {
+      const currentPlayer = $('.current-player')
+      self.movePlayer(currentPlayer, e.target)
+    })
   }
 }
 
 export default Game
-
