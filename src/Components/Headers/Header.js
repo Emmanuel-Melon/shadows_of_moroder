829968e4fd16b61b23b0b887965120c2c11ac4ad
Eman <emmanuelgatwech@gmail.com>
2020-02-25 19:47:46 +0300

    deleted unnecessary files?

--- src/Components/Headers/Header.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-import './Header.css'
-
-const Header = name => {
-  const header = $('<header></header>').addClass('header')
-  return header.html(name)
-}
-
-export default Header
